5f013d86df3b71618e53b5a7443d91e904a506b1
Billie Bakker <wimb64@icloud.com>
2025-08-06 14:33:13 +0200

    add dynamic title based on route

--- src/components/TopHeaderBar.vue
+++ src/components/TopHeaderBar.vue
@@ -1,14 +1,27 @@
-<script>
-export default {
-  name: 'TopHeaderBar',
-}
-</script>
-
 <template>
   <header class="h-[64px] shrink-0 w-full bg-ribbook-red flex justify-center items-center relative">
-    <div class="text-ribbook-yellow text-[32px] font-red-october">Ribbook</div>
-    <div class="absolute h-full left-[23px] top-0 flex justify-center items-center">
+    <div class="text-ribbook-yellow text-[30px] font-red-october">{{ currentTitle }}</div>
+    <div class="absolute h-full left-[14px] top-0 flex justify-center items-center">
       <span class="icon icon-40 icon-yellow">Menu</span>
     </div>
   </header>
 </template>
+
+<script>
+export default {
+  name: 'TopHeaderBar',
+  computed: {
+    currentTitle() {
+      // dynamische titel op basis van route
+      const map = {
+        home: 'Ribbook',
+        activities: 'Activiteiten',
+        account: 'Account',
+        login: 'Inloggen',
+      }
+      // Ribbook als geen match
+      return map[this.$route.name] || 'Ribbook'
+    },
+  },
+}
+</script>
