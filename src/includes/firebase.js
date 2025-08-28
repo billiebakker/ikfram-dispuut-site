3c3c0807290ce213bdde5a7f3d0e290b80b67c7f
Billie Bakker <wimb64@icloud.com>
2025-08-28 14:47:11 +0200

    update gitignore

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -3,13 +3,14 @@ import { collection, getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
 
+// check de firebase console voor deze config
 const firebaseConfig = {
-  apiKey: 'AIzaSyD904AbSGmQZAU-XVc8gYlKJvfgRM1tqJg',
-  authDomain: 'dispuuts-site.firebaseapp.com',
-  projectId: 'dispuuts-site',
-  storageBucket: 'dispuuts-site.firebasestorage.app',
-  // messagingSenderId: '393407453806',
-  appId: '1:393407453806:web:caeee640e2033fd145a47f',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  // messagingSenderId: '',
+  appId: '',
 }
 
 // Initialize Firebase
